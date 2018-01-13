21373d78149be98007a4ac3ee8f01a68c4fcaf78
Tony Mugendi <mugendi.tony26@gmail.com>
2018-01-13 16:08:36 +0300

    Setup components and firebase

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# employeemanager
+# Employee Manager
 
 > An Employee Manager
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+        <div id="navbar">
+                <h3>Dashboard</h3>
+        </div>
+  
+</template>
+
+<script>
+export default {
+        name: 'navbar',
+        data() {
+                return {
+
+                }
+        }
+  
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
