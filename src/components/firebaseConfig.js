3e26c5505ea42daf586063e03ad49b50fbfffbcd
Tony Mugendi <mugendi.tony26@gmail.com>
2018-01-13 14:08:11 +0300

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+        apiKey: "AIzaSyBR0oAYduOJZv3hlzNGOm92WPR_YQeFC5A",
+        authDomain: "employee-db-4dee0.firebaseapp.com",
+        databaseURL: "https://employee-db-4dee0.firebaseio.com",
+        projectId: "employee-db-4dee0",
+        storageBucket: "employee-db-4dee0.appspot.com",
+        messagingSenderId: "875027220894"
+}
